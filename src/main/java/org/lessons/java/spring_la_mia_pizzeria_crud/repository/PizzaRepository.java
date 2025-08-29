e757be0950ab66d09ae94519e12d3e40af4ba5ad
martinoalanza03 <lanzamartino3@gmail.com>
2025-08-29 12:20:13 +0200

    feat: add on interface findBytitle

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
@@ -1,8 +1,10 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.repository;
 
+import java.util.List;
+
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
-
+    List<Pizza> findByTitle(String title);
 }
