05e5ece104fa61e3d1592dceb1b69f0fc1c40438
martinoalanza03 <lanzamartino3@gmail.com>
2025-08-29 12:19:15 +0200

    feat: create scaffolding service

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/service/PizzaService.java
@@ -0,0 +1,11 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.service;
+
+import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PizzaService {
+
+    private PizzaRepository pizzaRepository;
+
+}
