ca9792ee47f45f04d99b5f0da0d4b21642f94e4d
martinoalanza03 <lanzamartino3@gmail.com>
2025-09-01 15:47:02 +0200

    feat: add username to index

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repository.IngredientRepository;
 import org.lessons.java.spring_la_mia_pizzeria_crud.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -30,9 +31,10 @@ public class PizzaController {
     private IngredientRepository ingredientRepository;
 
     @GetMapping
-    public String index(Model model) {
+    public String index(Authentication authentication, Model model) {
         List<Pizza> pizze = pizzaService.findAll();
         model.addAttribute("pizze", pizze);
+        model.addAttribute("username", authentication.getName());
         return "pizze/index";
     }
 
