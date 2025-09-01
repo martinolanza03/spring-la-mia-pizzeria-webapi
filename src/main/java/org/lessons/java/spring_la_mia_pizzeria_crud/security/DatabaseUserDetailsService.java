99a90790864ee28bb67c99acf21d25c011d2252c
martinoalanza03 <lanzamartino3@gmail.com>
2025-09-01 15:41:18 +0200

    feat: created user management with related roles

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/security/DatabaseUserDetailsService.java
@@ -0,0 +1,28 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.security;
+
+import java.util.Optional;
+
+import org.lessons.java.spring_la_mia_pizzeria_crud.model.User;
+import org.lessons.java.spring_la_mia_pizzeria_crud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class DatabaseUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userAttempt = userRepository.findByUsername(username);
+
+        if (userAttempt.isEmpty()) {
+            throw new UnsupportedOperationException("The are no users available with this username");
+        }
+
+        return new DatabaseUserDetails(userAttempt.get());
+    }
+
+}
---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/security/SecurityConfiguration.java
@@ -0,0 +1,51 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.security;
+
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers("/pizze/create", "/pizze/edit/**").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/pizze/**").hasAuthority("ADMIN")
+                .requestMatchers("/offers", "/offers/**").hasAuthority("ADMIN")
+                .requestMatchers("/ingredient", "/ingredient/**").hasAuthority("ADMIN")
+                .requestMatchers("/pizze/", "/pizze/**").hasAuthority("USER")
+                .requestMatchers("/**").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+    @SuppressWarnings("deprecation")
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    DatabaseUserDetailsService userDetailsService() {
+        return new DatabaseUserDetailsService();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
